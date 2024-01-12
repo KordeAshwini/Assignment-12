b2ab9f7816f51be83a069a42a41946f8e5ccf429
KordeAshwini <132747365+KordeAshwini@users.noreply.github.com>
2024-01-12 21:17:06 +0530

    Assignment12

---
+++ Question1_src/app/buginfo/buginfo.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-buginfo',
+  standalone: true,
+  imports: [],
+  templateUrl: './buginfo.component.html',
+  styleUrl: './buginfo.component.css'
+})
+export class BuginfoComponent 
+{
+  
+  
+
+}
---
+++ Question1_src/app/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent 
+{
+
+}
